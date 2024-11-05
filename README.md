f794002c31f56da2548a2a80385db65cdf709748
Rock-Roll1968 <ernesto.v.garcia@outlook.com>
2024-11-04 23:19:28 -0600

    Completed

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Leaflet-Challenge
-Interactive Visualizations HTML/JavaScript 
+
+
+## Website: 
+[website](git@github.com:Rock-Roll1968/Leaflet-Challenge.git)
+
+## Description
+Leaflet Earthquake Visualization
+
+![app_image](Map.png)
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+
+- [Features](#features)
+
+- [Contact](#contact)
+
+## Installation
+VS Code, Python, D3, GeoJSON, Leaflet URL, HTML, CSS
+
+## Usage
+Launch HTML with VS Code Live Server extension
+
+## Credits
+Ernesto Garcia
+
+
+
+## Features
+Interactive map with hover and click markers with details for tremor locations, magnitude and depth. 
+
+
+
+## Contact
+If there are any questions or concerns, I can be reached at:
+##### [github: ROCK-ROLL1968](https://github.com/ROCK-ROLL1968)
+##### [email: ernesto.v.garcia@outlook.com](mailto:ernesto.v.garcia@outlook.com)
--- static/js/logic.js
+++ static/js/logic.js
@@ -17,7 +17,7 @@ const init = async () => {
             return {
                 color: 'black',
                 weight: 1,
-                radius: mag * 2,
+                radius: mag * 3,
                 fillOpacity: .65,
                 fillColor:
                     depth < 10 ? 'green': 
@@ -40,6 +40,20 @@ const init = async () => {
     }).addTo(map);
     
 };
+
+var legend = L.control({ position: "bottomright" });
+legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    div.innerHTML += "<h4>Depth</h4>"
+    div.innerHTML += "<span style='background: green; width: 15px; height: 15px; display: inline-block;'></span> 0-10 km<br>";
+    div.innerHTML += "<span style='background: lime; width: 15px; height: 15px; display: inline-block;'></span> 10-30 km<br>";
+    div.innerHTML += "<span style='background: yellow; width: 15px; height: 15px; display: inline-block;'></span> 30-50 km<br>";
+    div.innerHTML += "<span style='background: orange; width: 15px; height: 15px; display: inline-block;'></span> 50-70 km<br>";
+    div.innerHTML += "<span style= 'background: darkOrange; width: 15px; height: 15px; display: inline-block;'></span> 70-90 km<br>";
+    div.innerHTML += "<span style= 'background: red; width: 15px; height: 15px; display: inline-block;'></span> 90 km<br>"
+    return div;
+};
+legend.addTo(map);
 
 
 init();
